a78dcdd63c176fbabfdbef0b92cdcc01794d401c
KUSHAL-31 <s.kushal31@gmail.com>
2022-07-06 12:16:48 +0530

    Edited landing page

--- js/main.js
+++ js/main.js
@@ -85,15 +85,5 @@
         return false;
     });
 
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        dots: true,
-        loop: true,
-        items: 1
-    });
-
 })(jQuery);
 
